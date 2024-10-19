7ccd71ce1a869bce60b96e858cedd09f548e7f69
shahdabdelfattah <shahdabdelfatah@outlook.com>
2024-10-19 18:37:49 +0300

    trial

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return <h2>Welcome to the Home Page!</h2>;
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <nav>
+        <Link to="/login">Login</Link>
+        <Link to="/register"> Register</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Home;
